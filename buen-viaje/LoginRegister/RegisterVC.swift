41a0f80706cab3b4aea1a216b7385e1b692e091e
Alexander <hello@alexander.works>
2020-09-13 00:55:42 +0300

    Set up Login and Register VCs

---
+++ buen-viaje/LoginRegister/RegisterVC.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterVC.swift
+//  buen-viaje
+//
+//  Created by Alexander on 12.09.2020.
+//  Copyright © 2020 Alexander Nuzhnyi. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
